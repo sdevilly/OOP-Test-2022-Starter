446b8408a71ff37315ab8dd1ba53f40545539931
Sean <sdevilly25@gmail.com>
2022-04-27 18:29:44 +0100

    Began attempting the visualisation of the nematodes

--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -10,11 +10,179 @@ public class NematodeVisualiser extends PApplet
 {
     ArrayList<nematode> nematodes = new ArrayList<nematode>();
 
+	public float edge;
+	public int i;
+
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
-		}		
+			if (i == 0)
+			{
+				i = 12;
+			}
+			else 
+			{
+				i--;
+			}
+		}	
+		
+		if (keyCode == RIGHT)
+		{
+			if (i == 12)
+			{
+				i = 0;
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+
+	void drawCircles()
+	{
+		stroke(255, 0, 0);
+		for(nematode n:nematodes)
+		{
+			// I forgot how to only read the first line of code so I had to hard code it
+			if(i==0)
+			{
+				float p = n.getLength();
+				for(int j=0;j<=2; j++) // hard coded
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+					stroke(255,0,0);
+				}
+			}
+			else if(i==1)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=4; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==2)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=6; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==3)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=2; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==4)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=3; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==5)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=1; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==6)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=5; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==7)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=7; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==8)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=3; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==9)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=0; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==10)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=5; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==11)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=5; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+			else if(i==12)
+			{
+				clear();
+				float p = n.getLength();
+				for(int j=0; j<=5; j++)
+				{
+					float x = map(j, 0, 8, edge, width - edge);
+					circle(width / 2, x, 80);
+				}
+			}
+		}
+
+		// for(j=0; j<=8; j++)
+		// { 
+		// float x = map(j, 0, 8, width - edge);
+		// circle(width / 2, x, 80);
+		// }
 	}
 
 
@@ -30,6 +198,8 @@ public class NematodeVisualiser extends PApplet
 		smooth();		
 		loadNematodes();
 		showNematodes();
+
+		edge = width * 0.1f;
 	}
 	
 	void showNematodes()
@@ -51,5 +221,6 @@ public class NematodeVisualiser extends PApplet
 
 	public void draw()
 	{	
+		drawCircles();
 	}
 }
